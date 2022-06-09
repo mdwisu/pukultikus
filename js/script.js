c1ea68ff6ccce66fe364022733d5d0e50db15110
mdwisu <dwisusanto784@gmail.com>
2022-06-09 11:36:01 +0700

    pukul dwi

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const tanah = document.querySelectorAll('.tanah');
 const tikus = document.querySelectorAll('.tikus');
+const papanSkor = document.querySelector('.papan-skor');
+const pop = document.querySelector('#pop');
 
 
 let tanahSebelumnya;
 let selesai;
+let skor;
 
 function randomTanah(tanah) {
     const t = Math.floor(Math.random() * tanah.length);
@@ -21,17 +24,35 @@ function randomWaktu(min, max) {
 
 function munculkanTikus() {
     const tRandom = randomTanah(tanah);
-    const wRandom = randomWaktu(300, 1000);
+    const wRandom = randomWaktu(1000, 2000);
     tRandom.classList.add('muncul');
     setTimeout(() => {
         tRandom.classList.remove('muncul');
-        munculkanTikus(tanah);
+        if (!selesai) {
+            munculkanTikus();
+        }
     }, wRandom);
 }
 
 function mulai() {
+    selesai = false;
+    skor = 0;
+    papanSkor.textContent = 0;
     munculkanTikus();
     setTimeout(() => {
-        
-    }, 10000);
+        selesai = true;
+    }, 20000);
 }
+
+function pukul() {
+    if (this.parentNode.classList.contains("muncul")) {
+        pop.play()
+        skor++;
+    }
+    this.parentNode.classList.remove('muncul');
+    papanSkor.textContent = skor;
+}
+
+tikus.forEach(t => {
+    t.addEventListener('click', pukul);
+});
